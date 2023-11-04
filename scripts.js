8204730c667e2f28c601d66585c8bacb36381569
Jada Walters <jada.walters@mymona.uwi.edu>
2023-11-04 17:27:23 -0500

    Added form functionality and images

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+    const newsForm = document.querySelector('form');
+    const emailInput = newsForm.querySelector('input');
+
+    let message = document.createElement('p');
+    let newDiv = document.createElement('div');
+
+    newsForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = emailInput.value;
+
+        if (email === ''){
+            event.preventDefault();
+            message.textContent = 'Please enter a valid email address';
+        }
+        else {
+            event.preventDefault();
+            message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+
+        newDiv.appendChild(message);
+        newsForm.appendChild(newDiv);
+        newsForm.insertAdjacentElement('afterend', newDiv);
+
+        emailInput.value = '';
+    });
+    
+});
